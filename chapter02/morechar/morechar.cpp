c81a0105d4fe6ca36e17ab9e65f76a1216441c7c
Ge Jian Feng <gejianfeng2@126.com>
2017-07-27 01:47:12 +0800

    + Add chapter02 code.

---
+++ chapter02/morechar/morechar.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	using namespace std;
+
+	char ch = 'M';
+	int i = ch;
+
+	cout << "The ASCII code for " << ch << " is " << i << endl;
+
+	cout << "Add one to the character code: " << endl;
+	ch = ch + 1;
+	i = ch;
+	cout << "The ASCII code for " << ch << " is " << i << endl;
+
+	cout << "Displaying char ch using cout.put(ch): ";
+	cout.put(ch);
+	cout.put('!');
+
+	cout << endl << "Done" << endl;
+
+	return 0;
+}
